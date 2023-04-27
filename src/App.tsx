1d0b83b19bcf174b316caa1c57ae44856ebbbfba
kurilee <sasukeliz@sina.cn>
2023-04-27 17:59:59 +0800

    code sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
 import { List } from '@arco-design/web-react';
-import { CmpFile } from './compornts/ArgComponets';
+import { CmpFile, CmpFolder, CmpText, CmpSwitch, CmpCombox, CmpNum } from './compornts/ArgComponets';
+import { appDataDir } from '@tauri-apps/api/path';
+import { useEffect, useState } from 'react';
+import './App.css'
+
+const optTypes = ["AdamW", "AdamW8bit", "Lion", "SGDNesterov", "SGDNesterov8bit", "DAdaptation", "AdaFactor"];
+const lrSchedulers = ["linear", "cosine", "cosine_with_restarts", "polynomial", "constant", "constant_with_warmup", "adafactor"];
 
 function App() {
+  const [defaultAppConfigPath, setDefaultAppConfigPath] = useState("");
+  useEffect(() => {
+    appDataDir().then(setDefaultAppConfigPath);
+  },[])
   return (
-    <div className="container">
-      <List style={{padding: "5px"}}>
-        <CmpFile title="基本模型" value="" defaultValue="D:\abc\1.safetensors" enable={true} isOptional={false} filters={[{ name: 'Checkpoint', extensions: ['safetensors', 'ckpt', 'pt']}]}></CmpFile>
-        <CmpFile title="VAE" value="" defaultValue="D:\abc\1.safetensors" enable={true} isOptional={true} filters={[{ name: 'VAE', extensions: ['safetensors', 'ckpt', 'pt']}]}></CmpFile>
-        <CmpFile title="训练集" value="" defaultValue="D:\abc\1.safetensors" enable={true} isOptional={false} filters={[]}></CmpFile>
-        <CmpFile title="输出路径" value="" defaultValue="D:\abc\1.safetensors" enable={true} isOptional={false} filters={[]}></CmpFile>
-      </List>
+    <div style={{padding: "5px"}}>
+        <List className="comp_list" size="small">
+          <CmpFile title="基本模型" value="" defaultValue="" enable={true} isOptional={false} filters={[{ name: 'Checkpoint', extensions: ['safetensors', 'ckpt', 'pt']}]}></CmpFile>
+          <CmpFile title="VAE" value="" defaultValue="" enable={true} isOptional={true} filters={[{ name: 'VAE', extensions: ['safetensors', 'ckpt', 'pt']}]}></CmpFile>
+          <CmpFolder title="训练集" value="" defaultValue="" enable={true} isOptional={false} defaultPath={defaultAppConfigPath} ></CmpFolder>
+          <CmpFolder title="输出路径" value="" defaultValue="" enable={true} isOptional={false} defaultPath={defaultAppConfigPath} ></CmpFolder>
+          <CmpText title='输出名称' value='' defaultValue='' enable={true} isOptional={false} ></CmpText>
+          <CmpNum title='Epoch保存间隔' value={1} defaultValue={1} enable={true} isOptional={false} min={1} max={30} step={0} precision={0} ></CmpNum>
+        </List>
+        <List className="comp_list" size="small">
+          <CmpSwitch title="xformers" value={true} defaultValue={true} enable={true} isOptional={true}></CmpSwitch>
+          <CmpSwitch title="enable-bucket" value={true} defaultValue={true} enable={true} isOptional={true}></CmpSwitch>
+          <CmpText title='SaveModelAs' value='safetensors' defaultValue='safetensors' enable={false} isOptional={false} ></CmpText>
+          <CmpText title='mixed_precision' value='fp16' defaultValue='fp16' enable={false} isOptional={false} ></CmpText>
+          <CmpText title='save_precision' value='fp16' defaultValue='fp16' enable={false} isOptional={false} ></CmpText>
+        </List>
+        <List className="comp_list" size="small">
+          <CmpText title="分辨率" value="" defaultValue="512,512" enable={true} isOptional={false}></CmpText>
+          <CmpCombox title="优化器" value='Lion' defaultValue='Lion' enable={true} isOptional={false} options={optTypes}></CmpCombox>
+          <CmpText title="网格类型" value='' defaultValue='networks.lora' enable={false} isOptional={false}></CmpText>
+          <CmpNum title='Epoch' value={10} defaultValue={10} enable={true} isOptional={false} min={1} max={300} step={1} precision={0}></CmpNum>
+          <CmpNum title='学习率' value={0.00001} defaultValue={0.00001} enable={true} isOptional={false} min={0.000001} max={0.001} step={0.000001} precision={6}></CmpNum>
+          <CmpNum title='UNetLr' value={0.00001} defaultValue={0.00001} enable={true} isOptional={false} min={0.000001} max={0.001} step={0.000001} precision={6}></CmpNum>
+          <CmpNum title='TextEncordLr' value={0.00001} defaultValue={0.00001} enable={true} isOptional={false} min={0.000001} max={0.001} step={0.000001} precision={6}></CmpNum>
+          <CmpNum title='NetworkDim' value={10} defaultValue={32} enable={true} isOptional={false} min={1} max={128} step={1} precision={0}></CmpNum>
+          <CmpNum title='NetworkAlpha' value={10} defaultValue={32} enable={true} isOptional={false} min={1} max={128} step={1} precision={0}></CmpNum>
+          <CmpNum title='训练批次' value={1} defaultValue={1} enable={true} isOptional={false} min={1} max={5} step={1} precision={0}></CmpNum>
+        </List>
+        <List className="comp_list" size="small">
+          <CmpCombox title="学习策略" value='cosine_with_restarts' defaultValue='cosine_with_restarts' enable={true} isOptional={false} options={lrSchedulers}></CmpCombox>
+          <CmpNum title='学习周期数' value={4} defaultValue={4} enable={true} isOptional={true} min={1} max={30} step={0} precision={0} ></CmpNum>
+        </List>
     </div>
   );
 }
--- src/compornts/ArgComponets.tsx
+++ src/compornts/ArgComponets.tsx
@@ -1,26 +1,57 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { Button, Checkbox, Input, Typography, Space } from '@arco-design/web-react'
+import { Button, Switch, Input, Typography, Space, Select, InputNumber } from '@arco-design/web-react'
 import { open } from '@tauri-apps/api/dialog'
 
-export interface CmpProps<T> {
+const titleWidth = "124px";
+const fullTitleWidth = "172px";
+const valueWidth = "500px";
+const componentSize = 'small';
+
+export interface CmpBase<T> {
     title: string,
     value: T,
     enable: boolean,
     isOptional: boolean,
     defaultValue: T,
+}
+export interface CmpFileProps extends CmpBase<string> {
     filters: Array<{ name: string, extensions: Array<string> }>
 }
+export interface CmpFolderProps extends CmpBase<string> {
+    defaultPath: string
+}
+export interface CmpTextProps extends CmpBase<string> {
+}
+export interface CmpSwitchProps extends CmpBase<boolean> {    
+}
+export interface CmpComboxProps extends CmpBase<string> {
+    options: Array<string>
+}
+export interface CmpNumProps extends CmpBase<number> {
+    min: number,
+    max: number,
+    step: number,
+    precision: number    
+}
 
-const CmpFile: React.FC<CmpProps<string>> = (props: CmpProps<string>) => {
-    const [title, setTitle] = useState("");
-    const [value, setValue] = useState("");
-    const [enable, setEnable] = useState(true);
-    const [isOptional, setIsOptional] = useState(false);
+const CmpFile: React.FC<CmpFileProps> = (props: CmpFileProps) => {
+    const [title, setTitle] = useState<string>("");
+    const [value, setValue] = useState<string>("");
+    const [enable, setEnable] = useState<boolean>(true);
+    const [isOptional, setIsOptional] = useState<boolean>(false);
+    useEffect(() => { 
+        setTitle(props.title);
+        setValue(props.defaultValue);
+        setEnable(props.enable);
+        setIsOptional(props.isOptional);
+    }, [])
 
     function showOptional() {
         if (isOptional) {
-            return (<Checkbox value={true} disabled={!enable}></Checkbox>);
+            return (<Switch size={componentSize} type="round" checked={enable} onClick={()=> {
+                setEnable(!enable);
+            }}></Switch>);
         }
     }
 
@@ -36,20 +67,202 @@ const CmpFile: React.FC<CmpProps<string>> = (props: CmpProps<string>) => {
         if (typeof(select) == 'string') setValue(select);
     }
 
+    return (<div>
+        <Space style={{margin: '2px'}}>
+        {showOptional()}
+        <Typography style={isOptional ? { width: titleWidth } : { width: fullTitleWidth }}>{title}</Typography>
+        <Input size='mini' type='text' style={{ width: valueWidth }} disabled={!enable} value={value} onChange={ (value: string, e: any) => { onValueChanged(value, e) } }></Input>
+        <Button size='mini' disabled={!enable} onClick={ async (e) => { await onOpenClicked() }}>Open</Button>
+        </Space>
+    </div>)
+};
+
+const CmpFolder: React.FC<CmpFolderProps> = (props: CmpFolderProps) => {
+    const [title, setTitle] = useState<string>("");
+    const [value, setValue] = useState<string>("");
+    const [enable, setEnable] = useState<boolean>(true);
+    const [isOptional, setIsOptional] = useState<boolean>(false);
     useEffect(() => { 
         setTitle(props.title);
         setValue(props.defaultValue);
         setEnable(props.enable);
         setIsOptional(props.isOptional);
     }, [])
+
+    function showOptional() {
+        if (isOptional) {
+            return (<Switch type="round" checked={enable} onClick={()=> {
+                setEnable(!enable);
+            }}></Switch>);
+        }
+    }
+
+    function onValueChanged(value: string, e: any) {
+
+    }
+
+    async function onOpenClicked() {
+        const select = await open({
+            directory: true,
+            defaultPath: props.defaultPath
+        })
+
+        if (typeof(select) == 'string') setValue(select);
+    }
+
     return (<div>
         <Space style={{margin: '2px'}}>
         {showOptional()}
-        <Typography style={isOptional ? { width: '73px' } : { width: '100px'}}>{title}</Typography>
-        <Input type='text' style={{ width: '500px' }} disabled={!enable} value={value} onChange={ (value: string, e: any) => { onValueChanged(value, e) } }></Input>
+        <Typography style={isOptional ? { width: titleWidth } : { width: fullTitleWidth }}>{title}</Typography>
+        <Input type='text' style={{ width: valueWidth }} disabled={!enable} value={value} onChange={ (value: string, e: any) => { onValueChanged(value, e) } }></Input>
         <Button disabled={!enable} onClick={ async (e) => { await onOpenClicked() }}>Open</Button>
         </Space>
     </div>)
-};
+}
 
-export { CmpFile };
+const CmpText: React.FC<CmpTextProps> = (props: CmpTextProps) => {
+    const [title, setTitle] = useState<string>("");
+    const [value, setValue] = useState<string>("");
+    const [enable, setEnable] = useState<boolean>(true);
+    const [isOptional, setIsOptional] = useState<boolean>(false);
+    useEffect(() => { 
+        setTitle(props.title);
+        setValue(props.defaultValue);
+        setEnable(props.enable);
+        setIsOptional(props.isOptional);
+    }, [])
+
+    function showOptional() {
+        if (isOptional) {
+            return (<Switch type="round" checked={enable} onClick={()=> {
+                setEnable(!enable);
+            }}></Switch>);
+        }
+    }
+
+    function onValueChanged(value: string, e: any) {
+
+    }
+
+    return (<div>
+        <Space style={{margin: '2px'}}>
+        {showOptional()}
+        <Typography style={isOptional ? { width: titleWidth } : { width: fullTitleWidth }}>{title}</Typography>
+        <Input type='text' style={{ width: valueWidth }} disabled={!enable} value={value} onChange={ (value: string, e: any) => { onValueChanged(value, e) } }></Input>
+        </Space>
+    </div>)
+}
+
+const CmpSwitch: React.FC<CmpSwitchProps> = (props: CmpSwitchProps) => {
+    const [title, setTitle] = useState<string>("");
+    const [value, setValue] = useState<boolean>(false);
+    const [enable, setEnable] = useState<boolean>(true);
+    const [isOptional, setIsOptional] = useState<boolean>(false);
+    useEffect(() => { 
+        setTitle(props.title);
+        setValue(props.defaultValue);
+        setEnable(props.enable);
+        setIsOptional(props.isOptional);
+    }, [])
+
+    function showOptional() {
+        if (isOptional) {
+            return (<Switch type="round" checked={enable} onClick={()=> {
+                setEnable(!enable);
+            }}></Switch>);
+        }
+    }
+
+    return (<div>
+        <Space style={{margin: '2px'}}>
+        {showOptional()}
+        <Typography style={isOptional ? { width: titleWidth } : { width: fullTitleWidth }}>{title}</Typography>
+        </Space>
+    </div>)
+}
+
+const CmpCombox: React.FC<CmpComboxProps> = (props: CmpComboxProps) => {
+    const [title, setTitle] = useState<string>("");
+    const [value, setValue] = useState<string>("");
+    const [enable, setEnable] = useState<boolean>(true);
+    const [isOptional, setIsOptional] = useState<boolean>(false);
+    const [options, setOptions] = useState<Array<string>>([]);
+
+    useEffect(() => { 
+        setTitle(props.title);
+        setValue(props.defaultValue);
+        setEnable(props.enable);
+        setIsOptional(props.isOptional);
+        setOptions(props.options);
+    }, [])
+
+    function showOptional() {
+        if (isOptional) {
+            return (<Switch type="round" checked={enable} onClick={()=> {
+                setEnable(!enable);
+            }}></Switch>);
+        }
+    }
+
+    function onValueChanged(value: string, e: any) {
+
+    }
+
+    return (<div>
+        <Space style={{margin: '2px'}}>
+        {showOptional()}
+        <Typography style={isOptional ? { width: titleWidth } : { width: fullTitleWidth }}>{title}</Typography>
+        <Select style={{ width: valueWidth }} disabled={!enable} value={value} onChange={ (value: string, e: any) => { onValueChanged(value, e) } }>
+            {
+                options.map((opt,idx) => {
+                    return (<Select.Option id={idx.toString()} value={opt}></Select.Option>)
+                })
+            }
+        </Select>
+        </Space>
+    </div>)
+}
+
+const CmpNum: React.FC<CmpNumProps> = (props: CmpNumProps) => {
+    const [title, setTitle] = useState<string>("");
+    const [value, setValue] = useState<number>(0);
+    const [enable, setEnable] = useState<boolean>(true);
+    const [isOptional, setIsOptional] = useState<boolean>(false);
+    const [min, setMin] = useState<number>(0);
+    const [max, setMax] = useState<number>(10);
+    const [step, setStep] = useState<number>(1);
+    const [precision, setPrecision] = useState<number>(1);
+
+    useEffect(() => { 
+        setTitle(props.title);
+        setValue(props.defaultValue);
+        setEnable(props.enable);
+        setIsOptional(props.isOptional);
+        setMin(props.min);
+        setMax(props.max);
+        setStep(props.step);
+        setPrecision(props.precision);
+    }, [])
+
+    function showOptional() {
+        if (isOptional) {
+            return (<Switch type="round" checked={enable} onClick={()=> {
+                setEnable(!enable);
+            }}></Switch>);
+        }
+    }
+
+    function onValueChanged(value: string, e: any) {
+
+    }
+
+    return (<div>
+        <Space style={{margin: '2px'}}>
+        {showOptional()}
+        <Typography style={isOptional ? { width: titleWidth } : { width: fullTitleWidth }}>{title}</Typography>
+        <InputNumber style={{ width: valueWidth }} mode='button' defaultValue={props.defaultValue} min={min} max={max} step={step} precision={precision} disabled={!enable}></InputNumber>
+        </Space>
+    </div>)
+}
+
+export { CmpFile, CmpFolder, CmpText, CmpSwitch, CmpCombox, CmpNum };
