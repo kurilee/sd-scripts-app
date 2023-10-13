72618316c8453670f8ee0d0965c997b70d855a0c
kurilee <sasukeliz@sina.cn>
2023-10-13 20:47:38 +0800

    滑动调整数值

--- src/compornts/BlockEditor.tsx
+++ src/compornts/BlockEditor.tsx
@@ -1,10 +1,10 @@
-import { useRef, useImperativeHandle } from 'react';
-import { useState, useEffect, forwardRef } from 'react';
+import { useRef, useImperativeHandle } from "react";
+import { useState, useEffect, forwardRef } from "react";
 
-import { RefInputType } from '@arco-design/web-react/es/Input/interface';
-import { Switch, Input, Typography, Space, InputNumber, Modal, Button, Slider } from '@arco-design/web-react';
+import { RefInputType } from "@arco-design/web-react/es/Input/interface";
+import { Switch, Input, Typography, Space, InputNumber, Modal, Button, Slider } from "@arco-design/web-react";
 
-import { CmpBaseRef, CmpBase, ComponentUtils } from './ArgComponets';
+import { CmpBaseRef, CmpBase, ComponentUtils } from "./ArgComponets";
 
 export interface BlockCellProps {
   lb: string;
@@ -18,7 +18,7 @@ export interface BlockCellRef {
   setValue(value: number): void;
 }
 const BlockCell = forwardRef<BlockCellRef, BlockCellProps>((props, ref) => {
-  const [lb, setlb] = useState<string>('');
+  const [lb, setlb] = useState<string>("");
   const [enable, setEnable] = useState<boolean>(false);
   const [value, setValue] = useState<number>(props.defaultValue);
   const inputRef = useRef<RefInputType>(null);
@@ -43,6 +43,7 @@ const BlockCell = forwardRef<BlockCellRef, BlockCellProps>((props, ref) => {
         size="mini"
         ref={inputRef}
         defaultValue={props.defaultValue}
+        value={value}
         min={0}
         max={128}
         step={1}
@@ -50,7 +51,8 @@ const BlockCell = forwardRef<BlockCellRef, BlockCellProps>((props, ref) => {
         disabled={!enable}
         onChange={(val) => {
           setValue(val);
-        }}></InputNumber>
+        }}
+      ></InputNumber>
     </Space>
   );
 });
@@ -58,10 +60,14 @@ const BlockCell = forwardRef<BlockCellRef, BlockCellProps>((props, ref) => {
 export interface CmpTextProps extends CmpBase<string> {}
 
 const BlockEditor = forwardRef<CmpBaseRef, CmpTextProps>((props, ref) => {
-  const [title, setTitle] = useState<string>('');
-  const [value, setValue] = useState<string>('');
+  const dim_min = 0;
+  const dim_max = 128;
+  const dim_default = 64;
+
+  const [title, setTitle] = useState<string>("");
+  const [value, setValue] = useState<string>("");
   const [enable, setEnable] = useState<boolean>(true);
-  const [defaultValue, setDefaultValue] = useState<string>('');
+  const [defaultValue, setDefaultValue] = useState<string>("");
   const [isOptional, setIsOptional] = useState<boolean>(false);
   const [modalVisible, setModalVisible] = useState<boolean>(false);
   const inputRef = useRef<RefInputType>(null);
@@ -78,9 +84,9 @@ const BlockEditor = forwardRef<CmpBaseRef, CmpTextProps>((props, ref) => {
 
   useEffect(() => {
     setTitle(props.title);
-    setValue(props.defaultValue ?? '');
+    setValue(props.defaultValue ?? "");
     setEnable(props.enable ?? true);
-    setDefaultValue(props.defaultValue ?? '');
+    setDefaultValue(props.defaultValue ?? "");
     setIsOptional(props.isOptional ?? false);
   }, []);
 
@@ -91,12 +97,12 @@ const BlockEditor = forwardRef<CmpBaseRef, CmpTextProps>((props, ref) => {
   }));
 
   const updateValueFromBlockEditor = (): void => {
-    let str = '';
+    let str = "";
     const arr_len = cellRefs.length;
     for (let i = 0; i < arr_len; i++) {
-      str += cellRefs[i].current?.getValue() ?? '';
+      str += cellRefs[i].current?.getValue() ?? "";
       if (i < arr_len - 1) {
-        str += ',';
+        str += ",";
       }
     }
 
@@ -105,9 +111,9 @@ const BlockEditor = forwardRef<CmpBaseRef, CmpTextProps>((props, ref) => {
   };
 
   const updateBlockEditorValue = (): void => {
-    if (value == '') return;
+    if (value == "") return;
 
-    const vals = value.split(',');
+    const vals = value.split(",");
     const val_len = vals.length;
     const ref_len = cellRefs.length;
     const min_len = Math.min(val_len, ref_len);
@@ -131,111 +137,169 @@ const BlockEditor = forwardRef<CmpBaseRef, CmpTextProps>((props, ref) => {
         onCancel={() => setModalVisible(false)}
         autoFocus={false}
         focusLock={true}
-        style={{ width: '54.5em' }}>
+        style={{ width: "54.5em" }}
+      >
         <Space direction="vertical" size={0}>
           <Space direction="horizontal">
             <Space direction="vertical" size={0}>
               <Space direction="horizontal" size={2}>
-                <BlockCell lb="IN0" defaultValue={0} enable={false} no={0} ref={cellRefs[0]} width='4em'/>
-                <BlockCell lb="IN1" defaultValue={128} enable={true} no={1} ref={cellRefs[1]} width='4em'/>
-                <BlockCell lb="IN2" defaultValue={128} enable={true} no={2} ref={cellRefs[2]} width='4em'/>
+                <BlockCell lb="IN0" defaultValue={0} enable={false} no={0} ref={cellRefs[0]} width="4em" />
+                <BlockCell lb="IN1" defaultValue={dim_default} enable={true} no={1} ref={cellRefs[1]} width="4em" />
+                <BlockCell lb="IN2" defaultValue={dim_default} enable={true} no={2} ref={cellRefs[2]} width="4em" />
               </Space>
-              <Slider min={0} max={128} onChange={(val) => {}}></Slider>
+              <Slider
+                min={dim_min}
+                max={dim_max}
+                defaultValue={dim_default}
+                onChange={(val) => {
+                  cellRefs[1].current?.setValue(Number(val));
+                  cellRefs[2].current?.setValue(Number(val));
+                }}
+              ></Slider>
             </Space>
 
-            <div style={{ width: '26em' }}></div>
+            <div style={{ width: "26em" }}></div>
             <Space direction="vertical" size={0}>
               <Space direction="horizontal" size={2}>
-                <BlockCell lb="OUT9" defaultValue={128} enable={true} no={22} ref={cellRefs[22]} width='4em'/>
-                <BlockCell lb="OUT10" defaultValue={128} enable={true} no={23} ref={cellRefs[23]} width='4em'/>
-                <BlockCell lb="OUT11" defaultValue={128} enable={true} no={24} ref={cellRefs[24]} width='4em'/>
+                <BlockCell lb="OUT9" defaultValue={dim_default} enable={true} no={22} ref={cellRefs[22]} width="4em" />
+                <BlockCell lb="OUT10" defaultValue={dim_default} enable={true} no={23} ref={cellRefs[23]} width="4em" />
+                <BlockCell lb="OUT11" defaultValue={dim_default} enable={true} no={24} ref={cellRefs[24]} width="4em" />
               </Space>
-              <Slider min={0} max={128} onChange={(val) => {}}></Slider>
+              <Slider
+                min={dim_min}
+                max={dim_max}
+                defaultValue={dim_default}
+                onChange={(val) => {
+                  cellRefs[22].current?.setValue(Number(val));
+                  cellRefs[23].current?.setValue(Number(val));
+                  cellRefs[24].current?.setValue(Number(val));
+                }}
+              ></Slider>
             </Space>
           </Space>
           <Space direction="horizontal">
-            <div style={{ width: '7.7em' }}></div>
-            <BlockCell lb="IN3" defaultValue={0} enable={false} no={3} ref={cellRefs[3]} width='4em' />
+            <div style={{ width: "7.7em" }}></div>
+            <BlockCell lb="IN3" defaultValue={0} enable={false} no={3} ref={cellRefs[3]} width="4em" />
           </Space>
           <Space direction="horizontal">
-            <div style={{ width: '7.7em' }}></div>
+            <div style={{ width: "7.7em" }}></div>
             <Space direction="vertical" size={0}>
               <Space direction="horizontal" size={2}>
-                <BlockCell lb="IN4" defaultValue={128} enable={true} no={4} ref={cellRefs[4]} width='4em' />
-                <BlockCell lb="IN5" defaultValue={128} enable={true} no={5} ref={cellRefs[5]} width='4em' />
+                <BlockCell lb="IN4" defaultValue={dim_default} enable={true} no={4} ref={cellRefs[4]} width="4em" />
+                <BlockCell lb="IN5" defaultValue={dim_default} enable={true} no={5} ref={cellRefs[5]} width="4em" />
               </Space>
-              <Slider min={0} max={128} onChange={(val) => {}}></Slider>
+              <Slider
+                min={dim_min}
+                max={dim_max}
+                defaultValue={dim_default}
+                onChange={(val) => {
+                  cellRefs[4].current?.setValue(Number(val));
+                  cellRefs[5].current?.setValue(Number(val));
+                }}
+              ></Slider>
             </Space>
 
-            <div style={{ width: '17.7em' }}></div>
+            <div style={{ width: "17.7em" }}></div>
             <Space direction="vertical" size={0}>
               <Space direction="horizontal" size={2}>
-                <BlockCell lb="OUT6" defaultValue={128} enable={true} no={19} ref={cellRefs[19]}  width='4em'/>
-                <BlockCell lb="OUT7" defaultValue={128} enable={true} no={20} ref={cellRefs[20]}  width='4em'/>
-                <BlockCell lb="OUT8" defaultValue={128} enable={true} no={21} ref={cellRefs[21]}  width='4em'/>
+                <BlockCell lb="OUT6" defaultValue={dim_default} enable={true} no={19} ref={cellRefs[19]} width="4em" />
+                <BlockCell lb="OUT7" defaultValue={dim_default} enable={true} no={20} ref={cellRefs[20]} width="4em" />
+                <BlockCell lb="OUT8" defaultValue={dim_default} enable={true} no={21} ref={cellRefs[21]} width="4em" />
               </Space>
-              <Slider min={0} max={128} onChange={(val) => {}}></Slider>
+              <Slider
+                min={dim_min}
+                max={dim_max}
+                defaultValue={dim_default}
+                onChange={(val) => {
+                  cellRefs[19].current?.setValue(Number(val));
+                  cellRefs[20].current?.setValue(Number(val));
+                  cellRefs[21].current?.setValue(Number(val));
+                }}
+              ></Slider>
             </Space>
           </Space>
           <Space direction="horizontal">
-            <div style={{ width: '11.85em' }}></div>
-            <BlockCell lb="IN6" defaultValue={0} enable={false} no={6} ref={cellRefs[6]}  width='4em'/>
+            <div style={{ width: "11.85em" }}></div>
+            <BlockCell lb="IN6" defaultValue={0} enable={false} no={6} ref={cellRefs[6]} width="4em" />
           </Space>
           <Space direction="horizontal">
-            <div style={{ width: '11.85em' }}></div>
+            <div style={{ width: "11.85em" }}></div>
             <Space direction="vertical" size={0}>
               <Space direction="horizontal" size={2}>
-                <BlockCell lb="IN7" defaultValue={128} enable={true} no={7} ref={cellRefs[7]}  width='4em'/>
-                <BlockCell lb="IN8" defaultValue={128} enable={true} no={8} ref={cellRefs[8]}  width='4em'/>
+                <BlockCell lb="IN7" defaultValue={dim_default} enable={true} no={7} ref={cellRefs[7]} width="4em" />
+                <BlockCell lb="IN8" defaultValue={dim_default} enable={true} no={8} ref={cellRefs[8]} width="4em" />
               </Space>
-              <Slider min={0} max={128} onChange={(val) => {}}></Slider>
+              <Slider
+                min={dim_min}
+                max={dim_max}
+                defaultValue={dim_default}
+                onChange={(val) => {
+                  cellRefs[7].current?.setValue(Number(val));
+                  cellRefs[8].current?.setValue(Number(val));
+                }}
+              ></Slider>
             </Space>
 
-            <div style={{ width: '9.4em' }}></div>
+            <div style={{ width: "9.4em" }}></div>
             <Space direction="vertical" size={0}>
               <Space direction="horizontal" size={2}>
-                <BlockCell lb="OUT3" defaultValue={128} enable={true} no={16} ref={cellRefs[16]}  width='4em'/>
-                <BlockCell lb="OUT4" defaultValue={128} enable={true} no={17} ref={cellRefs[17]}  width='4em'/>
-                <BlockCell lb="OUT5" defaultValue={128} enable={true} no={18} ref={cellRefs[18]}  width='4em'/>
+                <BlockCell lb="OUT3" defaultValue={dim_default} enable={true} no={16} ref={cellRefs[16]} width="4em" />
+                <BlockCell lb="OUT4" defaultValue={dim_default} enable={true} no={17} ref={cellRefs[17]} width="4em" />
+                <BlockCell lb="OUT5" defaultValue={dim_default} enable={true} no={18} ref={cellRefs[18]} width="4em" />
               </Space>
-              <Slider min={0} max={128} onChange={(val) => {}}></Slider>
+              <Slider
+                min={dim_min}
+                max={dim_max}
+                defaultValue={dim_default}
+                onChange={(val) => {
+                  cellRefs[16].current?.setValue(Number(val));
+                  cellRefs[17].current?.setValue(Number(val));
+                  cellRefs[18].current?.setValue(Number(val));
+                }}
+              ></Slider>
             </Space>
           </Space>
           <Space direction="horizontal">
-            <div style={{ width: '16em' }}></div>
-            <BlockCell lb="IN9" defaultValue={0} enable={false} no={9} ref={cellRefs[9]}  width='4em'/>
+            <div style={{ width: "16em" }}></div>
+            <BlockCell lb="IN9" defaultValue={0} enable={false} no={9} ref={cellRefs[9]} width="4em" />
           </Space>
           <Space direction="horizontal">
-            <div style={{ width: '16em' }}></div>
+            <div style={{ width: "16em" }}></div>
             <Space direction="vertical" size={0}>
               <Space direction="horizontal" size={2}>
-                <BlockCell lb="IN10" defaultValue={0} enable={false} no={10} ref={cellRefs[10]}  width='4em'/>
-                <BlockCell lb="IN11" defaultValue={0} enable={false} no={11} ref={cellRefs[11]}  width='4em'/>
+                <BlockCell lb="IN10" defaultValue={0} enable={false} no={10} ref={cellRefs[10]} width="4em" />
+                <BlockCell lb="IN11" defaultValue={0} enable={false} no={11} ref={cellRefs[11]} width="4em" />
               </Space>
             </Space>
 
-            <div style={{ width: '1em' }}></div>
+            <div style={{ width: "1em" }}></div>
             <Space direction="vertical" size={0}>
               <Space direction="horizontal" size={2}>
-                <BlockCell lb="OUT0" defaultValue={0} enable={false} no={13} ref={cellRefs[13]}  width='4em'/>
-                <BlockCell lb="OUT1" defaultValue={0} enable={false} no={14} ref={cellRefs[14]}  width='4em'/>
-                <BlockCell lb="OUT2" defaultValue={0} enable={false} no={15} ref={cellRefs[15]}  width='4em'/>
+                <BlockCell lb="OUT0" defaultValue={0} enable={false} no={13} ref={cellRefs[13]} width="4em" />
+                <BlockCell lb="OUT1" defaultValue={0} enable={false} no={14} ref={cellRefs[14]} width="4em" />
+                <BlockCell lb="OUT2" defaultValue={0} enable={false} no={15} ref={cellRefs[15]} width="4em" />
               </Space>
             </Space>
           </Space>
           <Space direction="horizontal">
-            <div style={{ width: '20.2em' }}></div>
+            <div style={{ width: "20.2em" }}></div>
             <Space direction="vertical" size={0}>
               <Space direction="horizontal" size={2}>
-                <BlockCell lb="MID" defaultValue={128} enable={true} no={12} ref={cellRefs[12]}  width='10.1em'/>
+                <BlockCell lb="MID" defaultValue={dim_default} enable={true} no={12} ref={cellRefs[12]} width="10.1em" />
               </Space>
-              <Slider min={0} max={128} onChange={(val) => {}}></Slider>
+              <Slider
+                min={dim_min}
+                max={dim_max}
+                defaultValue={dim_default}
+                onChange={(val) => {
+                  cellRefs[12].current?.setValue(Number(val));
+                }}
+              ></Slider>
             </Space>
-
           </Space>
         </Space>
       </Modal>
-      <Space style={{ margin: '2px' }}>
+      <Space style={{ margin: "2px" }}>
         {isOptional && <Switch size="small" type="round" checked={enable} onChange={setEnable}></Switch>}
         <Typography style={ComponentUtils.getTypStyle(isOptional)}>{title}</Typography>
         <Input size="mini" readOnly={true} ref={inputRef} type="text" style={{ width: ComponentUtils.valueWidth }} disabled={!enable} value={value} defaultValue={defaultValue} onChange={(v) => setValue(v)}></Input>
@@ -245,7 +309,8 @@ const BlockEditor = forwardRef<CmpBaseRef, CmpTextProps>((props, ref) => {
           onClick={() => {
             updateBlockEditorValue();
             setModalVisible(true);
-          }}>
+          }}
+        >
           Editor
         </Button>
       </Space>
